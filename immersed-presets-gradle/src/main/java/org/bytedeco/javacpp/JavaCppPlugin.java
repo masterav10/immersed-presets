2642cf7c2af819f4de00145c31d6706071394f1c
Daniel Avila <masterav10@gmail.com>
2021-09-01 11:34:19 -0400

    New plugin for javacpp.

---
+++ immersed-presets-gradle/src/main/java/org/bytedeco/javacpp/JavaCppPlugin.java
@@ -0,0 +1,14 @@
+package org.bytedeco.javacpp;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class JavaCppPlugin implements Plugin<Project>
+{
+    @Override
+    public void apply(Project project)
+    {
+        // TODO Auto-generated method stub
+
+    }
+}
