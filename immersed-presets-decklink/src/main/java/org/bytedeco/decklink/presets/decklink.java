ca98c04d91d1d614543c8d2571c17f246ebdca71
Daniel Avila <masterav10@gmail.com>
2021-09-16 12:10:22 -0400

    Resolved compilation issues related to REFIID.

--- immersed-presets-decklink/src/main/java/org/bytedeco/decklink/presets/decklink.java
+++ immersed-presets-decklink/src/main/java/org/bytedeco/decklink/presets/decklink.java
@@ -6,10 +6,12 @@ import org.bytedeco.javacpp.annotation.Properties;
 import org.bytedeco.javacpp.tools.Info;
 import org.bytedeco.javacpp.tools.InfoMap;
 import org.bytedeco.javacpp.tools.InfoMapper;
+import org.bytedeco.systems.presets.windows;
 
 //@formatter:off
 @Properties(
         inherit = {
+            windows.class,
             com.class,
             windef.class
         },
@@ -20,9 +22,12 @@ import org.bytedeco.javacpp.tools.InfoMapper;
             @Platform(
                 includepath = {
                     "C:/Tools/Blackmagic DeckLink SDK 12.1/Win/include"
-                }, 
+                },
                 include = {
                     "DeckLinkAPI_h.h"
+                },
+                cinclude = {
+                    "DeckLinkAPI_i.c"
                 }
             )
         }
@@ -32,14 +37,22 @@ public class decklink implements InfoMapper
 {   
     @Override
     public void map(InfoMap infoMap)
-    {           
+    {   
+        infoMap.clear();
+     
+        infoMap.put(new Info("REFIID")
+                .cppTypes("const GUID &"));
+        
+        infoMap.put(new Info("IID")
+                .cppTypes("GUID"));
+        
+        infoMap.put(new Info("__IID_DEFINED__")
+                .define(true));
+        
         infoMap.put(new Info("EXTERN_C", "MIDL_INTERFACE", "__clrcall", "extern \"C++\"")
                 .annotations().cppTypes());
         
         infoMap.put(new Info("BSTR")
                 .cast().valueTypes("CharPointer", "CharBuffer", "char[]").pointerTypes("PointerPointer"));
-        
-        infoMap.put(new Info("BEGIN_INTERFACE").cppText("#define BEGIN_INTERFACE"));
-        infoMap.put(new Info("END_INTERFACE").cppText("#define END_INTERFACE"));
     }
 }
