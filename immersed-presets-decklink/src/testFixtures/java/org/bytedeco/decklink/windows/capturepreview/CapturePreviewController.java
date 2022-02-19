40023a584d63f83cf8410d2a7cb5e104a9c52372
Daniel Avila <masterav10@gmail.com>
2022-02-19 01:19:56 -0500

    Added connection input support.

--- immersed-presets-decklink/src/testFixtures/java/org/bytedeco/decklink/windows/capturepreview/CapturePreviewController.java
+++ immersed-presets-decklink/src/testFixtures/java/org/bytedeco/decklink/windows/capturepreview/CapturePreviewController.java
@@ -1,8 +1,17 @@
 package org.bytedeco.decklink.windows.capturepreview;
 
+import static org.bytedeco.decklink.windows.ComSupport.*;
+import static org.bytedeco.global.decklink.*;
+import static org.bytedeco.global.windef.*;
+
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.bytedeco.decklink.IDeckLink;
+import org.bytedeco.decklink.IDeckLinkProfileAttributes;
 
 import javafx.application.Platform;
 import javafx.collections.ObservableList;
@@ -15,11 +24,27 @@ import javafx.scene.layout.Region;
 
 public class CapturePreviewController
 {
+    private static final Map<Integer, String> kInputConnections;
+
+    static
+    {
+        kInputConnections = new LinkedHashMap<>();
+        kInputConnections.put(bmdVideoConnectionSDI, "SDI");
+        kInputConnections.put(bmdVideoConnectionHDMI, "HDMI");
+        kInputConnections.put(bmdVideoConnectionOpticalSDI, "Optical SDI");
+        kInputConnections.put(bmdVideoConnectionComponent, "Component");
+        kInputConnections.put(bmdVideoConnectionComposite, "Composite");
+        kInputConnections.put(bmdVideoConnectionSVideo, "S-Video");
+    }
+
     @FXML
     private Canvas previewCanvas;
 
     @FXML
     private ComboBox<DeckLinkDevice> inputDeviceDropdown;
+
+    @FXML
+    private ComboBox<String> inputConnectionDropdown;
 
     @FXML
     public void initialize()
@@ -45,6 +70,51 @@ public class CapturePreviewController
         });
         inputDeviceDropdown.setButtonCell(inputDeviceDropdown.getCellFactory()
                                                              .call(null));
+
+        inputDeviceDropdown.getSelectionModel()
+                           .selectedItemProperty()
+                           .addListener((obs, ov, nv) -> onDeviceSelected(nv));
+    }
+
+    private void onDeviceSelected(DeckLinkDevice device)
+    {
+        IDeckLink deckLink = device.getDeckLinkInstance();
+        IDeckLinkProfileAttributes deckLinkAttributes = find(deckLink, IDeckLinkProfileAttributes.class);
+
+        if (deckLinkAttributes == null || deckLinkAttributes.isNull())
+            return;
+
+        long[] availableInputConnections = new long[1];
+
+        if (deckLinkAttributes.GetInt(BMDDeckLinkVideoInputConnections, availableInputConnections) != S_OK)
+            return;
+
+        long[] currentInputConnection = new long[1];
+        if (device.getDeckLinkConfiguration()
+                  .GetInt(bmdDeckLinkConfigVideoInputConnection, currentInputConnection) != S_OK)
+        {
+            currentInputConnection[0] = bmdVideoConnectionUnspecified;
+        }
+
+        final SelectionModel<String> model = this.inputConnectionDropdown.getSelectionModel();
+        final List<String> inputConnections = this.inputConnectionDropdown.getItems();
+        inputConnections.clear();
+
+        for (Entry<Integer, String> connection : kInputConnections.entrySet())
+        {
+            final int key = connection.getKey();
+            final String value = connection.getValue();
+
+            if ((availableInputConnections[0] & key) == 1)
+            {
+                inputConnections.add(value);
+
+                if (currentInputConnection[0] == key)
+                {
+                    model.select(value);
+                }
+            }
+        }
     }
 
     public void onDeviceAdded(IDeckLink device)
