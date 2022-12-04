c2ffa12707e06ea4719f169ca19a3b7409716a77
Daniel Avila <masterav10@gmail.com>
2022-12-04 00:46:02 -0500

    Adding new library for creating virtual controllers.

---
+++ immersed-presets-vigemclient/src/main/java/org/bytedeco/vigem/preset/vigemclient.java
@@ -0,0 +1,89 @@
+package org.bytedeco.vigem.preset;
+
+import java.util.List;
+
+import org.bytedeco.javacpp.ClassProperties;
+import org.bytedeco.javacpp.LoadEnabled;
+import org.bytedeco.javacpp.annotation.NoException;
+import org.bytedeco.javacpp.annotation.Platform;
+import org.bytedeco.javacpp.annotation.Properties;
+import org.bytedeco.javacpp.tools.Info;
+import org.bytedeco.javacpp.tools.InfoMap;
+import org.bytedeco.javacpp.tools.InfoMapper;
+import org.bytedeco.systems.presets.windows;
+
+//@formatter:off
+@Properties(
+      inherit = {
+          windows.class,
+      },
+      names = {"windows-x86_64"}, 
+      target = "org.bytedeco.vigemclient", 
+      global = "org.bytedeco.vigemclient.global.vigemclient",
+      value = {
+          @Platform(
+              includepath = "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.29.30133/include",
+              linkpath = "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.29.30133/lib/x64",
+              include = {
+                  "limits.h", "ViGEm/Common.h", "ViGEm/Client.h", "Internal.h"
+              },
+              link = {
+                  "ViGEmClient"
+              }
+          )
+      }
+)
+@NoException
+//@formatter:on
+public class vigemclient implements InfoMapper, LoadEnabled
+{
+    @Override
+    public void init(ClassProperties properties)
+    {
+        final String[] srcs = System.getProperty("nativelib.src")
+                                    .split(";");
+
+        List<String> includePaths = properties.get("platform.includepath");
+
+        for (String src : srcs)
+        {
+            if (!includePaths.contains(src))
+            {
+                includePaths.add(src);
+            }
+        }
+
+        final List<String> linkPaths = properties.get("platform.linkpath");
+
+        for (Object keyObj : System.getProperties()
+                                   .keySet())
+        {
+            final String key = keyObj.toString();
+
+            if (key.startsWith("nativelib.bin"))
+            {
+                final String value = System.getProperty(key);
+
+                if (!linkPaths.contains(value))
+                {
+                    linkPaths.add(value);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void map(InfoMap infoMap)
+    {
+        infoMap.put(new Info("VIGEM_API").cppTypes())
+               .put(new Info("VIGEM_DEPRECATED").annotations("@Deprecated").cppTypes())
+               .put(new Info("_I8_MIN", "_I8_MAX", "_UI8_MAX", "_I16_MIN", "_I16_MAX", "_UI16_MAX", 
+                             "_I32_MIN", "_I32_MAX", "_UI32_MAX", 
+                             "DEVICE_IO_CONTROL_END", "DEVICE_IO_CONTROL_BEGIN").skip())
+               .put(new Info("PVIGEM_TARGET").cast().valueTypes("PointerPointer<VIGEM_TARGET>"))
+               .put(new Info("PVIGEM_CLIENT").cast().valueTypes("PointerPointer<VIGEM_CLIENT>"))
+               .put(new Info("VIGEM_ERROR").cast().valueTypes("int"))
+               .put(new Info("VIGEM_TARGET_TYPE", "VIGEM_TARGET_STATE", "_VIGEM_ERRORS", "VIGEM_ERROR").enumerate());
+    }
+
+}
