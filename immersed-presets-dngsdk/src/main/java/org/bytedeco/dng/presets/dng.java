3f9f60d34aaaf30f540c0ede4e3c418524b08e3c
Daniel Avila <masterav10@gmail.com>
2024-08-29 13:12:25 -0400

    Initial implementation of dng library.

--- immersed-presets-dngsdk/src/main/java/org/bytedeco/dng/presets/dng.java
+++ immersed-presets-dngsdk/src/main/java/org/bytedeco/dng/presets/dng.java
@@ -7,14 +7,15 @@ import org.bytedeco.javacpp.LoadEnabled;
 import org.bytedeco.javacpp.annotation.NoException;
 import org.bytedeco.javacpp.annotation.Platform;
 import org.bytedeco.javacpp.annotation.Properties;
-import org.bytedeco.javacpp.presets.javacpp;
+import org.bytedeco.javacpp.tools.Info;
 import org.bytedeco.javacpp.tools.InfoMap;
 import org.bytedeco.javacpp.tools.InfoMapper;
+import org.bytedeco.systems.presets.windows;
 
 //@formatter:off
 @Properties(
         inherit = {
-            javacpp.class
+            windows.class
         },
         names = {"windows-x86_64"}, 
         target = "org.bytedeco.dng", 
@@ -22,6 +23,28 @@ import org.bytedeco.javacpp.tools.InfoMapper;
         value = {
             @Platform(
                 include = {
+                    "dng_uncopyable.h",
+                    // "dng_deprecated_flags.h",
+                    // "dng_flags.h",
+                    "dng_types.h",
+                    "dng_tag_values.h",
+                    "dng_tag_types.h",
+                    "dng_sdk_limits.h",
+                    "dng_rational.h",
+                    // "dng_pthread.h",
+                    "dng_mutex.h",
+                    "dng_errors.h",
+                    "dng_exceptions.h",
+                    "dng_safe_arithmetic.h",
+                    "dng_classes.h",
+                    "dng_memory.h",
+                    "dng_utils.h",
+                    "dng_string.h",
+                    "dng_point.h",
+                    "dng_auto_ptr.h",
+                    "dng_stream.h",
+                    "dng_fingerprint.h",
+                    "dng_area_task.h",
                     "dng_image_writer.h"
                 },
                 link = {
@@ -37,7 +60,19 @@ public class dng implements InfoMapper, LoadEnabled
     @Override
     public void map(InfoMap infoMap)
     {
+        infoMap.put(new Info(
+                "kMetadataSubset_CopyrightOnly", 
+                "kMetadataSubset_CopyrightAndContact",
+                "kMetadataSubset_All", 
+                "kMetadataSubset_AllExceptLocationInfo", 
+                "kMetadataSubset_AllExceptCameraInfo", 
+                "kMetadataSubset_AllExceptCameraAndLocation", 
+                "kMetadataSubset_AllExceptCameraRawInfo",
+                "kMetadataSubset_AllExceptCameraRawInfoAndLocation")
+               .cppTypes("int").translate(false));
 
+        infoMap.put(new Info("hypot", "DNG_ALWAYS_INLINE", "MULUH", "MULSH")
+               .cppTypes().annotations());
     }
 
     @Override
